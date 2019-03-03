015ca8b7a7ea0328794d2ee5d3df154d13e3c8ff
Vlad Koval <demossfen2@gmail.com>
2019-03-03 16:04:44 +0200

    init commit

---
+++ app/src/main/java/com/example/moneymanagement/AddItemActivity.kt
@@ -0,0 +1,14 @@
+package com.example.moneymanagement
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AddItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+
+      //  setTitle(R.string.add_item_title);
+    }
+}
